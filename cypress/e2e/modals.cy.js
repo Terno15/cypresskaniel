bbbd98885e60f8123483549b07e1d96bb853c869
Thierno Diallo <ternodialseny@gmail.com>
2024-08-11 22:44:37 +0200

    End of modal session

--- cypress/e2e/modals.cy.js
+++ cypress/e2e/modals.cy.js
@@ -3,12 +3,36 @@ describe('Cypress Modals', () => {
     it('Open And Assert Modal', ()=> {
         cy.visit('https://practice-automation.com/modals/')
         cy.contains('Click to see a simple modal.').should('be.visible')
-        cy.get('p').should('contain', 'Click to see a simple modal.')
+         ///Open the simple modal
+        cy.get('#simpleModal').should('contain', 'Simple Modal').click()
+        //cy.contains('Simple Modal').click()
+        //Check the popup result
+        cy.get('#pum_popup_title_1318').should('contain', 'Simple Modal')
+        cy.get('[class="pum-content popmake-content"]')
+            .find('p').should('be.visible')
+            .and('contain', 'Hi, I’m a simple modal.')
+            //Or
+            .contains('Hi, I’m a simple modal.')
     })
-    ///Open the simple modal
-    it('click on the simple modal', () => {
-        //cy.get('#simpleModal').should('contain', 'Simple Modal').click()
-        cy.contains('Simple Modal').click()
+           ///Second modaol
+    it.only('Second modal form', ()=> {
+        cy.visit('https://practice-automation.com/modals/')
 
+          //Data variables
+          const name = 'DIALLO'
+          const email = 'tad@gmail.com'
+          const message = 'I like learning cypress !'
+        cy.get('button').contains('Form Modal').click()
+        //cy.get('[class="popmake-674 pum-trigger"]').contains('Form Modal').should('be.visible')
+
+        cy.get('#pum_popup_title_674').contains('Modal Containing A Form')
+        cy.get('#g1051-name').type(name)
+        cy.get('#g1051-email').type(email)
+        cy.get('#contact-form-comment-g1051-message').type(message)
+        cy.get('.contact-submit')
+            .find('.pushbutton-wide')
+            .should('be.visible')
+            .click()
     })
+    
 })
