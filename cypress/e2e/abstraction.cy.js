25763408cbc86558ba762748c2d96b2179ae5ad9
Thierno Diallo <ternodialseny@gmail.com>
2024-08-16 20:28:16 +0200

    Object class 2

--- cypress/e2e/abstraction.cy.js
+++ cypress/e2e/abstraction.cy.js
@@ -6,21 +6,22 @@ describe('Cypress E2E Testing Demo', () => {
  beforeEach(() =>{
   const examplePage = new ExamplePage()
   examplePage.openExemplePage()
+  examplePage.assertTitle()
+  examplePage.assertTitle()
  })
 
  afterEach(() =>{
   cy.log('Running after each of my tests')
  })
 
- it('Assert Url', ()=>{
-  const toto = new ExamplePage()
-  toto.assertUrl()
- })
+//  it('Assert Url', ()=>{
+//   //const toto = new ExamplePage()
+//   //toto.assertUrl()
+//  })
 
- it('Assert Title', () => {
-  const AssertTitle = new ExamplePage()
-  AssertTitle.assertTitle()
- })
+//  it('Assert Title', () => {
+//   //const AssertTitle = new ExamplePage()
+//  })
 
  it('Assert Element', () =>{
   cy.get('h1').should('be.visible')
