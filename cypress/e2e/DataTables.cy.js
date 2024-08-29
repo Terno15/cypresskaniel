75c5a65dfeb343f9bd6b304b62aacc09e8dc3f13
Thierno Diallo <ternodialseny@gmail.com>
2024-08-29 19:02:00 +0200

    DATA TABLES

---
+++ cypress/e2e/DataTables.cy.js
@@ -0,0 +1,23 @@
+describe('DATA TABLES', () => {
+  it('passes', () => {
+    cy.visit('https://practice-automation.com/tables/')
+    cy.get('.container')
+        .find('h1')
+        .should('contain', 'Tables')
+    cy.get('.entry-content').find('p').should('be.visible')
+
+    cy.get('[class="wp-block-table"]').within(() =>{
+      cy.get('td').eq(0).should('contain', 'Item')
+      cy.get('td').eq(1).should('contain', 'Price')
+      cy.get('tr')
+            .should('contain', 'Oranges')
+            .and('contain', 'Laptop')
+            .and('contain', 'Marbles')
+            .should('contain', '$3.99')
+            .and('contain', '$1200.00')
+            .and('contain', '$1.25')
+    cy.get('tr').eq(1).should('contain', 'Oranges')
+    cy.get('tr').eq(2).should('contain', 'Laptop')
+    })
+  })
+})
